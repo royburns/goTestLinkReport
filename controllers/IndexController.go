44ea04ca28354e7a7b5a59d19a16ba888d8e8fee
Roy T.Burns <Royburns.Peng@software.dell.com>
2014-07-22 15:40:53 +0800

    forbid some code.

--- controllers/IndexController.go
+++ controllers/IndexController.go
@@ -1,10 +1,10 @@
 package controllers
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/astaxie/beego"
 	"github.com/royburns/goTestLinkReport/models"
-	"strconv"
+	// "strconv"
 	// "strings"
 )
 
@@ -17,44 +17,44 @@ func (this *IndexController) Get() {
 	ExecutionsTableHeader := models.GetExectutionTableHeader()
 
 	// Calculate pages.
-	pagenum := 20
-	pn, err := strconv.Atoi(this.Input().Get("p"))
-	maxPageNum := int(models.GetExecutionCount()/int64(pagenum)) + 1
-	if err != nil || pn > maxPageNum {
-		pn = 1
-	}
+	// pagenum := 20
+	// pn, err := strconv.Atoi(this.Input().Get("p"))
+	// maxPageNum := int(models.GetExecutionCount()/int64(pagenum)) + 1
+	// if err != nil || pn > maxPageNum {
+	// 	pn = 1
+	// }
 
-	if pn < 10 {
-		this.Data["Prev"] = 1
-	} else {
-		this.Data["Prev"] = pn - 10
-	}
+	// if pn < 10 {
+	// 	this.Data["Prev"] = 1
+	// } else {
+	// 	this.Data["Prev"] = pn - 10
+	// }
 
-	if pn > maxPageNum-10 {
-		this.Data["Next"] = maxPageNum
-	} else {
-		this.Data["Next"] = pn + 10
-	}
+	// if pn > maxPageNum-10 {
+	// 	this.Data["Next"] = maxPageNum
+	// } else {
+	// 	this.Data["Next"] = pn + 10
+	// }
 
-	// Get TestPlans
-	testplans := make(map[int]string)
-	res := models.GetAllTestPlans("v_testlink_testexecution_tree")
-	for key, item := range res {
-		testplans[key] = string(item["TestPlan"])
-	}
-	this.Data["TestPlans"] = testplans
+	// // Get TestPlans
+	// testplans := make(map[int]string)
+	// res := models.GetAllTestPlans("v_testlink_testexecution_tree")
+	// for key, item := range res {
+	// 	testplans[key] = string(item["TestPlan"])
+	// }
+	// this.Data["TestPlans"] = testplans
 
-	// Calculate page list.
-	this.Data["PageList"] = calPageList(pn, maxPageNum)
+	// // Calculate page list.
+	// this.Data["PageList"] = calPageList(pn, maxPageNum)
 
-	// 100, (page-1)*100
+	// // 100, (page-1)*100
 	// testexecution_tree, err := models.GetAllExecutions(pagenum, (pn-1)*pagenum)
 
-	if err != nil {
-		beego.Debug(fmt.Sprintf("Failed to get reports from db: %v\n", err))
-	} else {
-		beego.Debug("Success!!!")
-	}
+	// if err != nil {
+	// 	beego.Debug(fmt.Sprintf("Failed to get reports from db: %v\n", err))
+	// } else {
+	// 	beego.Debug("Success!!!")
+	// }
 
 	this.Data["TableHeader"] = ExecutionsTableHeader
 	// this.Data["TestExecutionTree"] = testexecution_tree
