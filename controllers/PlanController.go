222039b90d421ba9dccc41be1288d8af7142802a
Roy T.Burns <Royburns.Peng@software.dell.com>
2014-07-24 10:27:13 +0800

    1. Bug fixed: forget to cache the sprint name.

--- controllers/PlanController.go
+++ controllers/PlanController.go
@@ -30,46 +30,62 @@ func (this *PlanController) Get() {
 		return
 	}
 
-	key := "plan"
-	value, err := client.Get(key)
+	key1 := "sprintname"
+	key2 := "sp-plan"
+
+	value1, err := client.Get(key1)
+	if err != nil {
+		fmt.Println("Failed to get value: ", err)
+		return
+	}
+	value2, err := client.Get(key2)
 	if err != nil {
 		fmt.Println("Failed to get value: ", err)
 		return
 	}
 
 	// Get TestPlans
-	sprintname := make(map[int]string)
+	var sp []interface{}
 	var tp []interface{}
-	if value == nil {
+	if value1 == nil || value2 == nil {
 		fmt.Println("The plan is not exists. We will query it from mysql and then store them in redis.")
 
 		res := models.GetAllSprintNames()
-		for key, item := range res {
-			sprintname[key] = string(item["SprintName"])
-		}
+		for _, item := range res {
+			sp = append(sp, string(item["SprintName"]))
 
-		for i := 0; i < len(sprintname); i++ {
-			temp := models.GetAllPlansBySprintName(sprintname[i])
+			temp := models.GetAllPlansBySprintName(string(item["SprintName"]))
 			tp = append(tp, temp)
 		}
-
-		value, err := json.Marshal(tp)
+		value1, err := json.Marshal(sp)
 		if err != nil {
 			fmt.Println("Failed to marshal: ", err)
 		}
-		client.Set(key, value)
-		client.Expire(key, expiration)
+		client.Set(key1, value1)
+		client.Expire(key1, expiration)
+
+		value2, err := json.Marshal(tp)
+		if err != nil {
+			fmt.Println("Failed to marshal: ", err)
+		}
+		client.Set(key2, value2)
+		client.Expire(key2, expiration)
 	} else {
 		fmt.Println("The plan is exists. We will unmarshal them.")
 		// var temp planinfo
-		err := json.Unmarshal(value, &tp)
+		err := json.Unmarshal(value1, &sp)
 		if err != nil {
 			fmt.Println("Failed to unmarshal: ", err)
 			return
 		}
+		err2 := json.Unmarshal(value2, &tp)
+		if err2 != nil {
+			fmt.Println("Failed to unmarshal: ", err2)
+			return
+		}
 	}
 
-	this.Data["TableHeader"] = sprintname
+	this.Data["TableHeader"] = sp
 	this.Data["TestPlanTree"] = tp
 	this.Data["Website"] = "goTestLinkReport.org"
 	this.Data["Email"] = "roy.burns@163.com"
